6e845036a45d3740080585d8d39ded642d61bb39
Tent <yoyyay318@gmail.com>
2017-09-06 17:57:23 +0200

    talala

---
+++ PiouClient/PiouClient/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PiouClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+FileAssociationInfo fai = new FileAssociationInfo(".piou");
+    if (!fai.Exists)
+      {
+         fai.Create("Piou");
+
+         //Specify MIME type (optional)
+         fai.ContentType = "application/myfile";
+
+         //Programs automatically displayed in open with list
+         fai.OpenWithList = new string[]
+        { "notepad.exe", "wordpad.exe", "PiouClient.exe" };
+       }
+    ProgramAssociationInfo pai = new ProgramAssociationInfo(fai.ProgID);
+    if (!pai.Exists)
+      {
+         pai.Create
+         (
+         //Description of program/file type
+         "PIOU file"
+
+         new ProgramVerb
+              (
+              //Verb name
+              "Open",
+              //Path and arguments to use
+              @"C:\SomePath\MyApp.exe %1"
+              )
+            );
+
+         //optional
+         pai.DefaultIcon = new ProgramIcon(@"C:\PiouClient\twiiter-bird.ico
+       }
