67c4b8fb964f0046ef1ab29b2fa51fcaecca151b
Tent <yoyyay318@gmail.com>
2017-09-07 18:34:43 +0200

    oh no i regret making this

---
+++ PiouUninstaller.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Bye ! Piou will now uninstall ! :(
+pause
+del C:\piou\PiouClient\PiouClient\Form1.cs
+del C:\piou\PiouClient\PiouClient\Form1.Designer.cs
+del C:\piou\PiouClient\PiouClient\Form1.resx
+del C:\piou\PiouClient\PiouClient\PiouClent.csproj
+del C:\piou\PiouClient\PiouClient\piou-welcome.png
+del C:\piou\PiouClient\PiouClient\Program.cs
+del C:\piou\PiouClient\PiouClient\twitter-bird.ico
+del C:\piou\PiouClient\PiouClient\bin
+del C:\piou\PiouClient\PiouClient\obj
+del C:\piou\PiouClient\PiouClient\Properties
+echo Done. The program will now close.
+pause
